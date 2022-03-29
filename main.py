c976b49be1570c59c060f67ca935652548c6857e
Roman-Dembrovskyi <romadem2396@gmail.com>
2022-03-29 18:11:38 +0200

    genome_to_melody

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 from pyo import *
+from random import choices
 from genetic import generate_genome
 
 # GLOBAL
-BITS_PER_NOTE = 3
-BPM = 120  # placeholder
-genome_len = 4  # placeholder
+BITS_PER_NOTE = 3 # liczba bitów do kodowania noty ; 2^3 = 8
+BPM = 120
+# Genome = List[int]
+
+def note_from_bits(bits): # bits : [0,1,1]
+    for index , bit in enumerate(bits): # enumerate od bita : [(0,0),(1,1),(2,1)]
+        return sum(bit*pow(2, index)) # suma : wartość*2^index
+
+def genome_to_melody(genome, num_notes, num_bars):
+    notes = [genome[i * BITS_PER_NOTE:i * BITS_PER_NOTE + BITS_PER_NOTE] for i in range(num_notes * num_bars)] # notes : slicing genomu w obszarze 3*n:3*n+3
+    
+    note_lenght = 4 / num_notes # długość każdej noty
+
+    melody = {
+        "notes" : [],
+        "velocity" : []
+    }
+
+    for note in notes:
+        index = note_from_bits(note)
+
+        melody["notes"] += [index]
+        melody["velocity"] += [127]
+
+    return melody
 
 
-def genome_to_melody(genome):
-    return None
-
-
-def convert_melody_to_genome(melody, bpm):
+def convert_melody_to_genome(melody):
     events = list()
     for step in melody["notes"]:
         events.append(Events(
             midinote=EventSeq(step, occurrences=1),
             midivel=EventSeq(melody["velocity"], occurrences=1),
-            beat=EventSeq(melody["beat"], occurrences=1),
+            #beat=EventSeq(melody["beat"], occurrences=1),
             attack=0.001,
             decay=0.05,
             sustain=0.5,
             release=0.005,
-            bpm=bpm
+            bpm=BPM
         ))
-    return events
 
 
 if __name__ == '__main__':
