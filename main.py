49af1617b7f6cb2461c9d782efb3cf2c44c59e7f
Roman-Dembrovskyi <romadem2396@gmail.com>
2022-04-19 17:46:58 +0200

    saving midi

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from pyo import *
 from random import choices
 from genetic import generate_genome
+from midiutil import MIDIFile
+from datetime import datetime
 
 # GLOBAL
 BITS_PER_NOTE = 3 # liczba bitów do kodowania noty ; 2^3 = 8
 NUM_NOTES = 4
-NUM_BARS = 4
+NUM_BARS = 3
 BPM = 120
 genome_len = BITS_PER_NOTE*NUM_NOTES*NUM_BARS
 KEY = 'C'
@@ -49,6 +51,8 @@ def genome_to_melody(genome, num_notes, num_bars):
 
     return melody
 
+#jedna operacja - krzyżowanie, mutacja
+#zip = pdf + kod | opisać problemy
 
 def convert_melody_to_genome(melody, bpm):
     events = list()
@@ -66,7 +70,43 @@ def convert_melody_to_genome(melody, bpm):
     return events
 
 
-if __name__ == '__main__':
+def save_genome_to_midi(filename, Genome, NUM_BARS, NUM_NOTES, NUM_STEPS, key, scale, root, bpm):
+    melody = genome_to_melody(genome, NUM_BARS, NUM_NOTES, NUM_STEPS, key, scale, root)
+
+    if len(melody["notes"][0]) != len(melody["velocity"]):
+        raise ValueError
+
+    mf = MIDIFile(1)
+
+    track = 0
+    channel = 0
+
+    time = 0.0
+    mf.addTrackName(track, time, "Sample Track")
+    mf.addTempo(track, time, bpm)
+
+    for i, vel in enumerate(melody["velocity"]):
+        if vel > 0:
+            for step in melody["notes"]:
+                mf.addNote(track, channel, step[i], time, melody["beat"][i], vel)
+
+        time += melody["beat"][i]
+
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, "wb") as f:
+        mf.writeFile(f)
+
+
+population_size = 3
+
+def main(): 
+
+    folder = str(int(datetime.now().timestamp()))
+
+    population = [generate_genome(num_bars * num_notes * BITS_PER_NOTE) for _ in range(population_size)]
+
+    population_id = 0
+
     s = Server().boot()
 
     genome = generate_genome(genome_len)
@@ -77,3 +117,16 @@ if __name__ == '__main__':
         unit.play()
     s.start()
     time.sleep(20)
+
+    print("saving population midi …")
+    for i, genome in enumerate(population):
+        save_genome_to_midi(f"{folder}/{population_id}/{scale}-{key}-{i}.mid", genome, num_bars, num_notes, num_steps, pauses, key, scale, root, bpm)
+    print("done")
+
+    running = input("continue? [Y/n]") != "n"
+    # population = next_generation
+    # population_id += 1
+
+
+    if __name__ == '__main__':
+        main()
